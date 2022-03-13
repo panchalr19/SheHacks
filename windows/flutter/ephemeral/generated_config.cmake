5a9567d7dcb0448f8a47c3b327ffa30606f7ddbb
panchalr19 <68776176+panchalr19@users.noreply.github.com>
2022-03-13 08:35:00 +0530

    project

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\kirti\\Documents\\shehacks\\sh" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\kirti\\Documents\\shehacks\\sh"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\kirti\\Documents\\shehacks\\sh\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\kirti\\Documents\\shehacks\\sh"
+  "FLUTTER_TARGET=C:\\Users\\kirti\\Documents\\shehacks\\sh\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\kirti\\Documents\\shehacks\\sh\\.dart_tool\\package_config.json"
+)
